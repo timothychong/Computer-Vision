a3d36d74559c71324dc22623bfcf3d53c3860eca
Timothy Chong <timothychong@outlook.com>
2014-09-19 22:06:39 -0400

    Added bounding box and correlation template matching

--- Assignment2/Assignment2_part1/Assignment2_part1.cpp
+++ Assignment2/Assignment2_part1/Assignment2_part1.cpp
@@ -7,8 +7,8 @@
 	Assignment 2
 	--------------
 	This program:
-		a) Tracks an object by template matching 
-		b) Recognizes hand shapes or gestures and creates a graphical display 
+		a) Tracks an object by template matching
+		b) Recognizes hand shapes or gestures and creates a graphical display
 	--------------
 	PART A
 */
@@ -23,41 +23,88 @@
 using namespace cv;
 using namespace std;
 
+void correlation(Mat & src, Mat & templ, Mat & dest);
+void addBoundingBox(Mat &src, Point p, int width, int height);
+
 int main()
 {
-	VideoCapture cap(0); 
+	VideoCapture cap(0);
 
 	// if not successful, exit program
-    if (!cap.isOpened())  
+    if (!cap.isOpened())
     {
         cout << "Cannot open the video cam" << endl;
         return -1;
     }
-	
+
 	Mat frame;
+    Mat corr_mat;
+    Mat templ;
+
+    templ = imread("template.PNG", 3);
+
 	namedWindow("MyVideo",WINDOW_AUTOSIZE);
+	namedWindow("Correlation",WINDOW_AUTOSIZE);
+    imshow("Correlation", templ);
+
 	while (1)
     {
 		// read a new frame from video
-        bool bSuccess = cap.read(frame); 
+        bool bSuccess = cap.read(frame);
 
 		//if not successful, break loop
-        if (!bSuccess) 
+        if (!bSuccess)
         {
              cout << "Cannot read a frame from video stream" << endl;
              break;
         }
-		imshow("MyVideo", frame);
 
-		if (waitKey(30) == 27) 
+        Point p = Point(10,10);
+        correlation(frame, templ, corr_mat);
+        addBoundingBox(frame, p, 50, 100);
+		imshow("MyVideo", frame);
+        imshow("Correlation", corr_mat);
+
+		if (waitKey(30) == 27)
 		{
 			cout << "esc key is pressed by user" << endl;
-			break; 
+			break;
 		}
 
 	}
-	
+
 	cap.release();
 	return 0;
+}
+
+
+Point correlation(Mat & src, Mat & templ, Mat & dest) {
+    matchTemplate( src, templ, dest, CV_TM_CCOEFF_NORMED);
+
+}
+
+void addBoundingBox(Mat &src, Point p, int width, int height) {
+
+    Vec3b color = Vec3b(0,0,255);
+
+    int leftBound = (p.x - width/2);
+    leftBound = (leftBound < 0)? 0 : leftBound;
+    int rightBound = (p.x + width/2);
+    rightBound = (rightBound >= src.cols)? src.cols - 1 : rightBound;
+    int lowerBound = p.y - height/2;
+    lowerBound = (lowerBound < 0)? 0 : lowerBound;
+    int upperBound = p.y + height/2;
+    upperBound = (upperBound >= src.rows)? src.rows - 1 : upperBound;
+
+
+    for(int i = leftBound; i < rightBound; i++) {
+        src.at<Vec3b>(upperBound, i) = color;
+        src.at<Vec3b>(lowerBound, i) = color;
+    }
+
+    for(int i = lowerBound; i < upperBound; i++) {
+        src.at<Vec3b>(i, leftBound) = color;
+        src.at<Vec3b>(i, rightBound) = color;
+    }
 }
 

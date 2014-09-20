004df9065861b5deeefe5ea40d6f92e5223a3b4b
Timothy Chong <timothychong@outlook.com>
2014-09-20 19:51:55 -0400

    Added some functions from lab to use

--- Assignment2/Assignment2_part2/Assignment2_part2.cpp
+++ Assignment2/Assignment2_part2/Assignment2_part2.cpp
@@ -7,8 +7,8 @@
 	Assignment 2
 	--------------
 	This program:
-		a) Tracks an object by template matching 
-		b) Recognizes hand shapes or gestures and creates a graphical display 
+		a) Tracks an object by template matching
+		b) Recognizes hand shapes or gestures and creates a graphical display
 	--------------
 	PART B
 */
@@ -23,41 +23,130 @@
 using namespace cv;
 using namespace std;
 
+void mySkinDetect(Mat& src, Mat& dst);
+void myFrameDifferencing(Mat& prev, Mat& curr, Mat& dst);
+void myMotionEnergy(Vector<Mat> mh, Mat& dst);
+int myMax(int a, int b, int c);
+int myMin(int a, int b, int c);
+
+
 int main()
 {
-	VideoCapture cap(0); 
+	VideoCapture cap(0);
 
 	// if not successful, exit program
-    if (!cap.isOpened())  
+    if (!cap.isOpened())
     {
         cout << "Cannot open the video cam" << endl;
         return -1;
     }
-	
+
 	Mat frame;
+    Mat dst;
 	namedWindow("MyVideo",WINDOW_AUTOSIZE);
+	namedWindow("Result",WINDOW_AUTOSIZE);
+
 	while (1)
     {
 		// read a new frame from video
-        bool bSuccess = cap.read(frame); 
+        bool bSuccess = cap.read(frame);
 
 		//if not successful, break loop
-        if (!bSuccess) 
+        if (!bSuccess)
         {
              cout << "Cannot read a frame from video stream" << endl;
              break;
         }
+        cap.read(dst);
+        mySkinDetect(frame,dst);
 		imshow("MyVideo", frame);
+		imshow("Result", dst);
 
-		if (waitKey(30) == 27) 
+		if (waitKey(30) == 27)
 		{
 			cout << "esc key is pressed by user" << endl;
-			break; 
+			break;
 		}
 
 	}
-	
+
 	cap.release();
 	return 0;
 }
 
+//Function that detects whether a pixel belongs to the skin based on RGB values
+void mySkinDetect(Mat& src, Mat& dst) {
+    //Surveys of skin color modeling and detection techniques:
+    //Vezhnevets, Vladimir, Vassili Sazonov, and Alla Andreeva. "A survey on pixel-based skin color detection techniques." Proc. Graphicon. Vol. 3. 2003.
+    //Kakumanu, Praveen, Sokratis Makrogiannis, and Nikolaos Bourbakis. "A survey of skin-color modeling and detection methods." Pattern recognition 40.3 (2007): 1106-1122.
+    for (int i = 0; i < src.rows; i++){
+        for (int j = 0; j < src.cols; j++){
+            //For each pixel, compute the average intensity of the 3 color channels
+            Vec3b intensity = src.at<Vec3b>(i,j); //Vec3b is a vector of 3 uchar (unsigned character)
+            int B = intensity[0]; int G = intensity[1]; int R = intensity[2];
+            if ((R > 95 && G > 40 && B > 20) && (myMax(R,G,B) - myMin(R,G,B) > 15) && (abs(R-G) > 15) && (R > G) && (R > B)){
+                //dst.at<uchar>(i,j) = 255;
+                dst.at<Vec3b>(i,j)[0] =255;
+                dst.at<Vec3b>(i,j)[1] =255;
+                dst.at<Vec3b>(i,j)[2] =255;
+            }
+        }
+    }
+}
+
+//Function that does frame differencing between the current frame and the previous frame
+void myFrameDifferencing(Mat& prev, Mat& curr, Mat& dst) {
+    //For more information on operation with arrays: http://docs.opencv.org/modules/core/doc/operations_on_arrays.html
+    //For more information on how to use background subtraction methods: http://docs.opencv.org/trunk/doc/tutorials/video/background_subtraction/background_subtraction.html
+    absdiff(prev, curr, dst);
+    Mat gs = dst.clone();
+    cvtColor(dst, gs, CV_BGR2GRAY);
+    dst = gs > 50;
+    Vec3b intensity = dst.at<Vec3b>(100,100);
+}
+
+//Function that accumulates the frame differences for a certain number of pairs of frames
+void myMotionEnergy(Vector<Mat> mh, Mat& dst) {
+    Mat mh0 = mh[0];
+    Mat mh1 = mh[1];
+    Mat mh2 = mh[2];
+
+    for (int i = 0; i < dst.rows; i++){
+        for (int j = 0; j < dst.cols; j++){
+            if (mh0.at<uchar>(i,j) == 255 || mh1.at<uchar>(i,j) == 255 ||mh2.at<uchar>(i,j) == 255){
+                dst.at<uchar>(i,j) = 255;
+            }
+        }
+    }
+}
+//Function that returns the maximum of 3 integers
+int myMax(int a, int b, int c) {
+	int m = a;
+    (void)((m < b) && (m = b));
+    (void)((m < c) && (m = c));
+     return m;
+}
+
+//Function that returns the minimum of 3 integers
+int myMin(int a, int b, int c) {
+	int m = a;
+    (void)((m > b) && (m = b));
+    (void)((m > c) && (m = c));
+     return m;
+}
+
+
+void correlation(Mat & src, Mat & templ, Mat & dest, Point & max, float & maxIntensity) {
+    matchTemplate( src, templ, dest, CV_TM_CCOEFF_NORMED);
+    maxIntensity = 0;
+    float temp = 0;
+    for(int x = 0; x < dest.cols; x ++ ) {
+        for(int y = 0; y < dest.rows; y ++ ) {
+            temp = dest.at<float>(y, x);
+            if (temp > maxIntensity && temp > 0.6) {
+                maxIntensity = temp;
+                max = Point(x, y);
+            }
+        }
+    }
+}

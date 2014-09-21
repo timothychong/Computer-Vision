7649b46aaa4b0e1b914c0807808a1303bf4f89e4
Timothy Chong <timothychong@outlook.com>
2014-09-20 21:06:28 -0400

    Basically done with part 2

--- Assignment2/Assignment2_part2/Assignment2_part2.cpp
+++ Assignment2/Assignment2_part2/Assignment2_part2.cpp
@@ -28,6 +28,7 @@ void myFrameDifferencing(Mat& prev, Mat& curr, Mat& dst);
 void myMotionEnergy(Vector<Mat> mh, Mat& dst);
 int myMax(int a, int b, int c);
 int myMin(int a, int b, int c);
+void correlation(Mat & src, Mat & templ, Mat & dest, Point & max, float & maxIntensity);
 
 
 int main()
@@ -45,18 +46,38 @@ int main()
     Mat dst;
 	namedWindow("MyVideo",WINDOW_AUTOSIZE);
 	namedWindow("Result",WINDOW_AUTOSIZE);
-	namedWindow("paper",WINDOW_AUTOSIZE);
-
-
 
     //Templates
     Mat paper = imread("template_paper.PNG", CV_LOAD_IMAGE_COLOR);
     Mat paper_binary = Mat::zeros(paper.rows, paper.cols, CV_8UC1);
     mySkinDetect(paper, paper_binary);
-    imshow("paper", paper_binary);
-    //Mat rock = imread("template_rock.PNG", CV_LOAD_IMAGE_COLOR);
-    //Mat scissors = imread("template_scissors.PNG", CV_LOAD_IMAGE_COLOR);
 
+    Mat rock = imread("template_rock.PNG", CV_LOAD_IMAGE_COLOR);
+    Mat rock_binary = Mat::zeros(rock.rows, rock.cols, CV_8UC1);
+    mySkinDetect(rock, rock_binary);
+
+    Mat scissors = imread("template_scissors.PNG", CV_LOAD_IMAGE_COLOR);
+    Mat scissors_binary = Mat::zeros(scissors.rows, scissors.cols, CV_8UC1);
+    mySkinDetect(scissors, scissors_binary);
+
+    vector<Mat> templates;
+    templates.push_back(paper_binary);
+    templates.push_back(rock_binary);
+    templates.push_back(scissors_binary);
+	//namedWindow("paper",WINDOW_AUTOSIZE);
+	//namedWindow("rock",WINDOW_AUTOSIZE);
+	//namedWindow("scissors",WINDOW_AUTOSIZE);
+    //imshow("paper", paper);
+    //imshow("rock", rock);
+    //imshow("scissors", scissors);
+
+    vector<Vec3b> colors = vector<Vec3b>();
+    Vec3b red = Vec3b(0,0,255);
+    Vec3b blue = Vec3b(255,0,0);
+    Vec3b green = Vec3b(0,255,0);
+    colors.push_back(red);
+    colors.push_back(blue);
+    colors.push_back(green);
 	while (1)
     {
 		// read a new frame from video
@@ -68,8 +89,50 @@ int main()
              cout << "Cannot read a frame from video stream" << endl;
              break;
         }
+
         dst = Mat::zeros(frame.rows, frame.cols, CV_8UC1);
+
+
+        cout << endl;
         mySkinDetect(frame,dst);
+        Point p;
+        float maxIntensity;
+        Mat dest;
+        float finalMax = 0;
+        float finalIndex = -1;
+        for (int i = 0 ; i < 3; i++) {
+            p = Point(-1,-1);
+            maxIntensity = 0;
+            correlation(dst, templates[i], dest, p,  maxIntensity);
+            if ( i == 2) {
+                maxIntensity *= 1.2;
+            }
+            if ( maxIntensity > finalMax && maxIntensity > 0.6) {
+                finalMax = maxIntensity;
+                finalIndex = i;
+            }
+
+            if ( i == 0 ) {
+                cout << "Paper Matching Correlation: \t" << maxIntensity << endl;
+
+            } else if ( i == 1) {
+                cout << "Rock Matching Correlation: \t" << maxIntensity << endl;
+
+            } else if ( i == 2) {
+                cout << "Scissors Matching Correlation: \t" << maxIntensity << endl;
+
+            }
+        }
+        cout << endl;
+
+        if (finalIndex != -1) {
+            for (int i = 0; i < 100; i ++ ) {
+                for (int j = 0; j < 100; j ++ ) {
+                    frame.at<Vec3b>(i, j) = colors[finalIndex];
+                }
+            }
+        }
+        //cout << i << " " << maxIntensity << endl;
 		imshow("MyVideo", frame);
 		imshow("Result", dst);
 
@@ -151,7 +214,7 @@ void correlation(Mat & src, Mat & templ, Mat & dest, Point & max, float & maxInt
     for(int x = 0; x < dest.cols; x ++ ) {
         for(int y = 0; y < dest.rows; y ++ ) {
             temp = dest.at<float>(y, x);
-            if (temp > maxIntensity && temp > 0.6) {
+            if (temp > maxIntensity && temp > 0.1) {
                 maxIntensity = temp;
                 max = Point(x, y);
             }

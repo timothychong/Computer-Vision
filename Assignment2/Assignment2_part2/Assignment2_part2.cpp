67dae17b1c136017ce6f3b50349da2620f3988f2
TheDuckCow <theduckcow@live.com>
2014-09-21 16:10:56 -0400

    updated UI and improved matching

--- Assignment2/Assignment2_part2/Assignment2_part2.cpp
+++ Assignment2/Assignment2_part2/Assignment2_part2.cpp
@@ -20,6 +20,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 
+
 using namespace cv;
 using namespace std;
 
@@ -34,6 +35,10 @@ void correlation(Mat & src, Mat & templ, Mat & dest, Point & max, float & maxInt
 int main()
 {
 	VideoCapture cap(0);
+	// reading image sequence for hand position,
+	// was going to have animated sequence, but was running oo slow.
+	//VideoCapture ani;
+	//ani.open("animation/animation_%04d.png");
 
 	// if not successful, exit program
     if (!cap.isOpened())
@@ -46,8 +51,12 @@ int main()
     Mat dst;
 	namedWindow("MyVideo",WINDOW_AUTOSIZE);
 	namedWindow("Result",WINDOW_AUTOSIZE);
+	moveWindow("MyVideo", 0,200);
+	moveWindow("Result",660,200);
 
-    //Templates
+	Mat ani_frame = imread("animation/animation_0005.png", CV_LOAD_IMAGE_COLOR);
+    
+	//Templates
     Mat paper = imread("template_paper.PNG", CV_LOAD_IMAGE_COLOR);
     Mat paper_binary = Mat::zeros(paper.rows, paper.cols, CV_8UC1);
     mySkinDetect(paper, paper_binary);
@@ -60,17 +69,13 @@ int main()
     Mat scissors_binary = Mat::zeros(scissors.rows, scissors.cols, CV_8UC1);
     mySkinDetect(scissors, scissors_binary);
 
+	// template vector, to easily add more or swap different hand shapes
     vector<Mat> templates;
     templates.push_back(paper_binary);
-    templates.push_back(rock_binary);
     templates.push_back(scissors_binary);
-	//namedWindow("paper",WINDOW_AUTOSIZE);
-	//namedWindow("rock",WINDOW_AUTOSIZE);
-	//namedWindow("scissors",WINDOW_AUTOSIZE);
-    //imshow("paper", paper);
-    //imshow("rock", rock);
-    //imshow("scissors", scissors);
+    templates.push_back(rock_binary);
 
+	// debug testing colors
     vector<Vec3b> colors = vector<Vec3b>();
     Vec3b red = Vec3b(0,0,255);
     Vec3b blue = Vec3b(255,0,0);
@@ -78,70 +83,114 @@ int main()
     colors.push_back(red);
     colors.push_back(blue);
     colors.push_back(green);
+	
+	// veriables related to GUI and feedback
+	int watchdog =0;
+	int state=0;
+	int animation_frame;
+	int animation_set;
+	int frequency = 0;
+
+	// this is used for saying which gesture was last active.
+	// -1 no shape, 0 = paper, 1=Scissors, 2=Rock
+	float finalIndex = -1;
+
+	// loop for every frame of the video
 	while (1)
     {
+		// this level runs every frame
+		
 		// read a new frame from video
-        bool bSuccess = cap.read(frame);
-
+		bool bSuccess = cap.read(frame);
 		//if not successful, break loop
-        if (!bSuccess)
-        {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
-        }
+		if (!bSuccess)
+		{
+			 cout << "Cannot read a frame from video stream" << endl;
+			 break;
+		}
 
-        dst = Mat::zeros(frame.rows, frame.cols, CV_8UC1);
+		if (watchdog >= frequency){
+			// this level only runs ever n frames
+			watchdog = 0;
+			
+			dst = Mat::zeros(frame.rows, frame.cols, CV_8UC1);
+
+			cout << endl;
+			mySkinDetect(frame,dst);
+			Point p;
+			float maxIntensity;
+			Mat dest;
+			float finalMax = 0;
+			finalIndex = -1;
+			for (int i = 0 ; i < 3; i++) {
+				p = Point(-1,-1);
+				maxIntensity = 0;
+				correlation(dst, templates[i], dest, p,  maxIntensity);
 
 
-        cout << endl;
-        mySkinDetect(frame,dst);
-        Point p;
-        float maxIntensity;
-        Mat dest;
-        float finalMax = 0;
-        float finalIndex = -1;
-        for (int i = 0 ; i < 3; i++) {
-            p = Point(-1,-1);
-            maxIntensity = 0;
-            correlation(dst, templates[i], dest, p,  maxIntensity);
-            if ( i == 2) {
-                maxIntensity *= 1.2;
-            }
-            if ( maxIntensity > finalMax && maxIntensity > 0.6) {
-                finalMax = maxIntensity;
-                finalIndex = i;
-            }
+				if ( i == 0 ) {
+					maxIntensity *= 1.25; // correlation adjustment 1.8
+					cout << "Paper Matching Correlation: \t" << maxIntensity << endl;
 
-            if ( i == 0 ) {
-                cout << "Paper Matching Correlation: \t" << maxIntensity << endl;
+				} else if ( i == 1) {
+					maxIntensity *= 1.2; // corelation adjustment 1.8
+					cout << "Scissors Matching Correlation: \t" << maxIntensity << endl;
 
-            } else if ( i == 1) {
-                cout << "Rock Matching Correlation: \t" << maxIntensity << endl;
+				} else if ( i == 2) {
+					maxIntensity *= 1; // corelation adjustment 0.3
+					cout << "Rock Matching Correlation: \t" << maxIntensity << endl;
 
-            } else if ( i == 2) {
-                cout << "Scissors Matching Correlation: \t" << maxIntensity << endl;
+				}
 
-            }
-        }
-        cout << endl;
+				if ( maxIntensity > finalMax && maxIntensity > 0.6) {
+					finalMax = maxIntensity;
+					finalIndex = i;
+				}
 
-        if (finalIndex != -1) {
-            for (int i = 0; i < 100; i ++ ) {
-                for (int j = 0; j < 100; j ++ ) {
-                    frame.at<Vec3b>(i, j) = colors[finalIndex];
-                }
-            }
-        }
-        //cout << i << " " << maxIntensity << endl;
+			}
+			cout << endl;
+
+			//cout << i << " " << maxIntensity << endl;
+			imshow("Result", dst);
+
+			
+		}
+
+		// for testing debug colos, easy visualizaiton (change colo block fr matching)
+		/*
+		if (finalIndex != -1) {
+			for (int i = 0; i < 100; i ++ ) {
+				for (int j = 0; j < 100; j ++ ) {
+					frame.at<Vec3b>(i, j) = colors[finalIndex];
+				}
+			}
+		}
+		*/
+
+		// below runs every frame
+
+		// overlay the frame of he different hand jestures on the output image.
+		ani_frame.copyTo(frame(Rect(170,frame.rows-100,300,100)));//,ani_frame,mask_image);
+		// showing which one was selected
+		int tmp = finalIndex;
+		if (finalIndex != -1){
+			for (int x=0; x<100; x++){
+				for (int y=0; y<100; y++){
+					//Vec3b pixel = frame.at<Vec3b>(170+x*100,y);
+					frame.at<Vec3b>(y+frame.rows-100,170+x+finalIndex*100)[0] = 150;
+					//mask_image.at<uchar>(y, x+100*tmp) = 255;
+				}
+			}
+		}
+
+		watchdog++;
 		imshow("MyVideo", frame);
-		imshow("Result", dst);
-
+		
 		if (waitKey(30) == 27)
 		{
 			cout << "esc key is pressed by user" << endl;
 			break;
 		}
-
 	}
 
 	cap.release();
@@ -158,9 +207,12 @@ void mySkinDetect(Mat& src, Mat& dst) {
             //For each pixel, compute the average intensity of the 3 color channels
             Vec3b intensity = src.at<Vec3b>(i,j); //Vec3b is a vector of 3 uchar (unsigned character)
             int B = intensity[0]; int G = intensity[1]; int R = intensity[2];
-            if ((R > 95 && G > 40 && B > 20) && (myMax(R,G,B) - myMin(R,G,B) > 15) && (abs(R-G) > 15) && (R > G) && (R > B)){
+            if ((R > 75 && G > 40 && B > 20) && (myMax(R,G,B) - myMin(R,G,B) > 15) && (abs(R-G) > 15) && (R > G) && (R > B)){
                 dst.at<uchar>(i,j) = 255;
             }
+			//if ((R > 95 && G > 40 && B > 20) && (myMax(R,G,B) - myMin(R,G,B) > 15) && (abs(R-G) > 15) && (R > G) && (R > B)){
+            //    dst.at<uchar>(i,j) = 255;
+            //}
         }
     }
 }

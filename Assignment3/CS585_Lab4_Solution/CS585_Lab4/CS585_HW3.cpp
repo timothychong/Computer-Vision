e5f04952870e37547531905aa4c8984e4041cc07
Timothy Chong <timothychong@outlook.com>
2014-09-29 23:37:09 -0400

    Thresholding sort of working

--- Assignment3/CS585_Lab4_Solution/CS585_Lab4/CS585_HW3.cpp
+++ Assignment3/CS585_Lab4_Solution/CS585_Lab4/CS585_HW3.cpp
@@ -32,114 +32,63 @@ void FindBinaryLargeObjects(const Mat &binaryImg, vector <vector<Point2i>> &blob
 int main(int argc, char **argv)
 {
 	// read image as grayscale
-    Mat img = imread("blob.png", 0); 
+    Mat img = imread("bcancer1.png");
     if(!img.data) {
         cout << "File not found" << std::endl;
         return -1;
     }
+    //Converting to grayscale
+    Mat im_gray;
+    cvtColor(img, im_gray,CV_RGB2GRAY);
 
-	// create windows
-	namedWindow("Original");
-    namedWindow("binary");
-    namedWindow("labelled");
-	imshow("Original",img);
-	// perform morphological operations to remove small objects (noise) and fill black spots inside objects
-	// %TODO
-	// Documentation on erode: http://docs.opencv.org/modules/imgproc/doc/filtering.html?highlight=erode#erode
-	// Documentation on dilate: http://docs.opencv.org/modules/imgproc/doc/filtering.html?highlight=erode#dilate
-	
-	// initialize structuring element for morphological operations
-	int erosion_size = 3;
-	int dilation_size = 3;
+	blur( img, img, Size(5,5) );
+    Mat img_bw;
+    threshold(im_gray, img_bw, 220.0, 255.0, THRESH_BINARY);
+
+
+	int erosion_size = 2;
+	int dilation_size = 2;
 	Mat element = getStructuringElement( MORPH_RECT,
                                        Size( 2*erosion_size + 1, 2*erosion_size+1 ),
                                        Point( erosion_size, erosion_size ) );
 	//perform erosions and dilations
-	erode(img, img, element);
-	dilate( img, img, element );
-	dilate( img, img, element );
+    erode(img_bw, img_bw, element);
+    dilate( img_bw, img_bw, element );
 
-	//convert thresholded image to binary image
-    Mat binary;
-    threshold(img, binary, 0.0, 1.0, THRESH_BINARY);
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+    Mat temp = img_bw.clone();
+    findContours(temp, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
-	//initialize vector to store all blobs
-	vector <vector<Point2i>> blobs;
 
-	//call function that detects blobs and modifies the input binary image to store blob info
-    FindBinaryLargeObjects(binary, blobs);
+	cout << "The number of contours detected is: " <<  contours.size() << endl;
 
-	//display the output
-	Mat output = Mat::zeros(img.size(), CV_8UC3);
-    // Randomly color the blobs
-    for(size_t i=0; i < blobs.size(); i++) {
-        unsigned char r = 255 * (rand()/(1.0 + RAND_MAX));
-        unsigned char g = 255 * (rand()/(1.0 + RAND_MAX));
-        unsigned char b = 255 * (rand()/(1.0 + RAND_MAX));
+	int maxsize = 0;
+	int maxind = 0;
+	Rect boundrec;
+	for(int i = 0; i < contours.size(); i++ )
+	{
+		// Documentation on contourArea: http://docs.opencv.org/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html#
+		double area = contourArea(contours[i]);
+		if (area > maxsize) {
+			maxsize = area;
+			maxind = i;
+			boundrec = boundingRect(contours[i]);
+		}
+	}
 
-        for(size_t j=0; j < blobs[i].size(); j++) {
-            int x = blobs[i][j].x;
-            int y = blobs[i][j].y;
+	Mat contour_output = Mat::zeros( img.size(), CV_8UC3 );
+    drawContours(contour_output, contours, maxind, 	Scalar(255, 0, 0), CV_FILLED, 8, hierarchy, 0);
+	//drawContours(contour_output, contours, maxind, Scalar(0,0,255), 2, 8, hierarchy);
+    //rectangle(contour_output, boundrec, Scalar(0,255,0),1, 8,0);
 
-            output.at<Vec3b>(y,x)[0] = b;
-            output.at<Vec3b>(y,x)[1] = g;
-            output.at<Vec3b>(y,x)[2] = r;
-        }
-    }
+	// create windows
+	namedWindow("Original");
+	namedWindow("Contour");
+    imshow("Original",img_bw);
+    imshow("Contour",contour_output);
 
 	//show the binary image, as well as the labelled image
-    imshow("binary", img);
-    imshow("labelled", output);
     waitKey(0);
-
     return 0;
 }
-
-void FindBinaryLargeObjects(const Mat &binary, vector <vector<Point2i>> &blobs)
-{
-	//clear blobs vector
-    blobs.clear();
-
-	//labelled image of type CV_32SC1
-	Mat label_image;
-    binary.convertTo(label_image, CV_32SC1);
-
-	//label count starts at 2
-    int label_count = 2; 
-
-	//iterate over all pixels until a pixel with a 1-value is encountered
-    for(int y=0; y < label_image.rows; y++) {
-        int *row = (int*)label_image.ptr(y);
-        for(int x=0; x < label_image.cols; x++) {
-			
-            if(row[x] != 1) {
-                continue;
-            }
-
-			//floodFill the connected component with the label count
-			//floodFill documentation: http://docs.opencv.org/modules/imgproc/doc/miscellaneous_transformations.html#floodfill
-            Rect rect;
-            floodFill(label_image, Point(x,y), label_count, &rect, 0, 0, 4);
-
-			//store all 2D co-ordinates in a vector of 2d points called blob
-            vector <Point2i> blob;
-            for(int i=rect.y; i < (rect.y+rect.height); i++) {
-                int *row2 = (int*)label_image.ptr(i);
-                for(int j=rect.x; j < (rect.x+rect.width); j++) {
-                    if(row2[j] != label_count) {
-                        continue;
-                    }
-                    blob.push_back(Point2i(j,i));
-                }
-            }
-			//store the blob in the vector of blobs
-            blobs.push_back(blob);
-
-			//increment counter
-            label_count++;
-        }
-    }
-	//cout << "The number of blobs in the image is: " << label_count;
-	//Code derived from: http://nghiaho.com/
-}
-

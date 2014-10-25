3a03ee3c08bd150320725b8d718d97c4d7b70834
Timothy Chong <timothychong@outlook.com>
2014-10-25 14:53:35 -0400

    Basically working

--- Assignment4/Assignment4_Part1/Assignment4_Part1.cpp
+++ Assignment4/Assignment4_Part1/Assignment4_Part1.cpp
@@ -1,7 +1,7 @@
 /**
 	CS585_Assignment4.cpp
-	@author: 
-	@version: 
+	@author:
+	@version:
 
 	CS585 Image and Video Computing Fall 2014
 	Assignment 4: BATS
@@ -11,13 +11,22 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/video/tracking.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <math.h>
+#include <stdio.h>
+#include "filterBundle.h"
+#include <limits>
+
 
 using namespace cv;
 using namespace std;
+
+#define width 1024
+#define height 1024
 
 /**
 	Reads the label maps contained in the text file and converts the label maps
@@ -38,40 +47,231 @@ void convertFileToMat(String filename, Mat& labelled, Mat& binary);
 	@param binary3channel The 3 channel image corresponding to the 1 channel binary image outputted
 		by convertFileToMat. The object detection positions will be drawn on this image
 */
-void drawObjectDetections(String filename, Mat& binary3channel);
+void drawObjectDetections(vector<FilterBundle> data, Mat& binary3channel);
+void centroidsFromFile(String filename, vector<vector<int>> & data);
+void convertFileToMatWithColor(String filename, Mat& output, vector<FilterBundle> & bundles);
+void printMat(Mat & mat);
+
+struct greater
+{
+    template<class T>
+        bool operator()(T const &a, T const &b) const { return a > b; }
+        };
 
 int main()
 {
-	String filename_seg = "Segmentation_Bats/CS585Bats-Segmentation_frame000000750.txt";
-	Mat labelled, binary;
-	convertFileToMat(filename_seg, labelled, binary);
+    srand(time(0));
 
-	String filename_det = "Localization_Bats/CS585Bats-Localization_frame000000750.txt";
-	Mat binary3C;
-	cvtColor(binary, binary3C, CV_GRAY2BGR);
-	drawObjectDetections(filename_det, binary3C);
+    vector <String> files, binary_files;
+    const int number_of_digits = 3;
+    const int number_of_frames = 20;
+    const int start_frame = 750;
 
-	namedWindow( "Labelled", 1 );
-	imshow("Labelled", labelled);
-	namedWindow( "Binary", 1 );
-	imshow("Binary", binary);
-	namedWindow( "Detections", 1 );
-	imshow("Detections", binary3C);
+    for ( int i= start_frame; i <  start_frame + number_of_frames; i ++ ) {
+        String str = "Localization_Bats/CS585Bats-Localization_frame000000";
+        String str_binary = "Segmentation_Bats/CS585Bats-Segmentation_frame000000";
+        int digit = (i == 0)? 2 : number_of_digits - 1 - (int)log10(i);
 
-	char key = waitKey(0);
-	return 0;
+        ostringstream ss;
+        ss << i;
+
+        str.append(string(digit, '0'));
+        str_binary.append(string(digit, '0'));
+        str.append(string(ss.str()));
+        str_binary.append(string(ss.str()));
+        str.append(".txt");
+        str_binary.append(".txt");
+
+        files.push_back(str);
+        binary_files.push_back(str_binary);
+    }
+    vector<FilterBundle> globalBundles;
+
+    vector<vector<int>> initialVec;
+    centroidsFromFile(files[0], initialVec);
+
+    for(int a = 0; a < initialVec.size(); a++) {
+        globalBundles.push_back(FilterBundle(Point(initialVec[a][0],initialVec[a][1])));
+    }
+    //Mat labelled, binary;
+    //convertFileToMat(binary_files[0], labelled, binary);
+    //cout << binary.rows << " " << binary.cols << endl;
+
+    for (int i = 1; i < files.size(); i++) {
+        //Mat labelled, binary;
+        //convertFileToMat(binary_files[i], labelled, binary);
+        //imshow(str, binary);
+        vector<vector<int>> vec;
+        centroidsFromFile(files[i], vec);
+
+        vector<int> tempGlobal;
+        for (int a = 0; a < globalBundles.size(); a++ ){
+            tempGlobal.push_back(a);
+        }
+        vector <int> usedDots;
+
+        //For every dots in the previous state
+        for (int j = 0; j < globalBundles.size(); j++ ) {
+            Point previousPrediction = globalBundles[j].getCurrentPrediction();
+            //Make a prediction of the new dot
+            Point prediction = globalBundles[j].predict();
+            //cout << "Working on " << previousPrediction.x << " " << previousPrediction.y << " Predict to be: " << prediction.x << " " << prediction.y << endl;
+            //If the prediction if out of bound. Don't look for new dot
+            if (prediction.x >= width || prediction.y >= height || prediction.x < 0 || prediction.y < 0){
+                //cout << "Object going out of bound" << endl;
+                continue;
+            }
+
+            float shortestDistance = numeric_limits<float>::max();
+            float shortestIndex = -1;
+
+            //Loop through every single new dot
+            for (int k = 0; k < vec.size(); k++) {
+                    //See if the dot has already been taken
+                    if(find(usedDots.begin(), usedDots.end(), k) != usedDots.end()) {
+                        continue;
+                    }
+                    //Get the new dot's location
+                    Point location = Point(vec[k][0], vec[k][1]);
+
+                    //Calculate distance and update shortest distance
+                    float distance = sqrt( (prediction.x - location.x) * (prediction.x - location.x) + (prediction.y - location.y) * (prediction.y - location.y));
+                    //if (distance < 2000 && distance < shortestDistance){
+                    if (distance < shortestDistance){
+                        shortestDistance = distance;
+                        shortestIndex = k;
+                    }
+            }
+            //If a shortest distance is found
+            if (shortestIndex != -1){
+                Point measurement = Point(vec[shortestIndex][0], vec[shortestIndex][1]);
+                //cout << "Updating: " << previousPrediction.x << " " << previousPrediction.y << " to " << measurement.x << " "  << measurement.y << endl;;
+                //Take it out from the leftovers (to be deleted)
+                tempGlobal.erase(remove(tempGlobal.begin(), tempGlobal.end(), j), tempGlobal.end());
+                globalBundles[j].currentIndex = shortestIndex + 1;
+                Point estimation = globalBundles[j].update(measurement);
+                usedDots.push_back(shortestIndex);
+            }
+            //} else
+                    //cout << "Can't find shortest distance" << endl;
+        }
+            //Sort that so that deletion won't be messed up
+            sort(tempGlobal.begin(), tempGlobal.end(), greater());
+
+            //Removing left overs
+            for (int a = 0; a < tempGlobal.size(); a++) {
+                globalBundles.erase(globalBundles.begin() + tempGlobal[a]);
+            }
+            //Adding new bodies
+            for (int k = 0; k < vec.size(); k++) {
+                if(find(usedDots.begin(), usedDots.end(), k) != usedDots.end()) {
+                    continue;
+                }
+                globalBundles.push_back(FilterBundle(Point(vec[k][0],vec[k][1])));
+            }
+            for (unsigned int a = 0; a < globalBundles.size(); a++ ) {
+                //cout << globalBundles[a] << endl;
+            }
+            //Mat canvas = Mat::zeros(height, width, CV_8UC3);
+            //drawObjectDetections(globalBundles, canvas);
+            Mat output;
+            convertFileToMatWithColor(binary_files[i], output, globalBundles);
+            //drawObjectDetections(globalBundles, output);
+            ostringstream ss;
+            ss << i << ".jpg";
+            imwrite(string(ss.str()), output);
+            //imwrite(string(ss.str()), canvas);
+            cout << "Finished - " << i << endl;
+
+        }
+
+        //int abc = 0;
+        //cin >> abc;
+
+        return 0;
+
+        char key = waitKey(0);
+        return 0;
+    }
+
+    void printMat(Mat & mat) {
+        for (int i = 0; i < mat.rows ; i++ ) {
+            for (int j = 0; j < mat.cols ; j++ ) {
+                cout << mat.at<float>(i,j) << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void convertFileToMatWithColor(String filename, Mat& output, vector<FilterBundle> & bundles)
+    {
+        //read file
+        ifstream infile(filename);
+        vector <vector <int>> data;
+        if (!infile){
+            cout << "Error reading file!";
+            return;
+        }
+
+        //read the comma separated values into a vector of vector of ints
+        while (infile)
+        {
+            string s;
+            if (!getline( infile, s )) break;
+
+            istringstream ss(s);
+            vector <int> datarow;
+
+            while (ss)
+            {
+              string srow;
+              int sint;
+              if (!getline( ss, srow, ',' )) break;
+              sint = atoi(srow.c_str()); //convert string to int
+              datarow.push_back(sint);
+            }
+            data.push_back(datarow);
+        }
+
+        //construct the labelled matrix from the vector of vector of ints
+        Mat labelled = Mat::zeros(data.size(), data.at(0).size(), CV_8UC1);
+
+        for (int i = 0; i < labelled.rows; ++i)
+            for (int j = 0; j < labelled.cols; ++j)
+                labelled.at<uchar>(i,j) = data.at(i).at(j);
+
+        map<int, Scalar> colorMap;
+        for(int a = 0; a < bundles.size(); a++ ) {
+            int p = bundles[a].currentIndex;
+            colorMap[p] = bundles[a].color;
+        }
+
+
+        //construct the binary matrix from the labelled matrix
+        output = Mat::zeros(labelled.rows, labelled.cols, CV_8UC3);
+        for (int i = 0; i < labelled.rows; ++i)
+            for (int j = 0; j < labelled.cols; ++j){
+                if (labelled.at<uchar>(i,j) != 0){
+                    if(colorMap.find(data.at(i).at(j)) != colorMap.end()){
+                        Scalar color = colorMap[data.at(i).at(j)];
+                        output.at<Vec3b>(i,j)[0] = color[0];
+                        output.at<Vec3b>(i,j)[1] = color[1];
+                        output.at<Vec3b>(i,j)[2] = color[2];
+                    } else {
+                        output.at<Vec3b>(i,j)[0] = 255;
+                        output.at<Vec3b>(i,j)[1] = 255;
+                        output.at<Vec3b>(i,j)[2] = 255;
+                }
+            }
+        }
 }
 
-void convertFileToMat(String filename, Mat& labelled, Mat& binary)
-{
-	//read file
+void centroidsFromFile(String filename, vector<vector<int>> & data) {
 	ifstream infile(filename);
-	vector <vector <int>> data;
 	if (!infile){
 		cout << "Error reading file!";
 		return;
 	}
-
 	//read the comma separated values into a vector of vector of ints
 	while (infile)
 	{
@@ -91,56 +291,17 @@ void convertFileToMat(String filename, Mat& labelled, Mat& binary)
 		}
 		data.push_back(datarow);
 	}
-
-	//construct the labelled matrix from the vector of vector of ints
-	labelled = Mat::zeros(data.size(), data.at(0).size(), CV_8UC1);
-	for (int i = 0; i < labelled.rows; ++i)
-		for (int j = 0; j < labelled.cols; ++j)
-			labelled.at<uchar>(i,j) = data.at(i).at(j);
-	
-	//construct the binary matrix from the labelled matrix
-	binary = Mat::zeros(labelled.rows, labelled.cols, CV_8UC1);
-	for (int i = 0; i < labelled.rows; ++i)
-		for (int j = 0; j < labelled.cols; ++j)
-			binary.at<uchar>(i,j) = (labelled.at<uchar>(i,j) == 0) ? 0: 255;
-
 }
 
-void drawObjectDetections(String filename, Mat& binary3channel)
+void drawObjectDetections(vector<FilterBundle> data, Mat& binary3channel)
 {
-	ifstream infile(filename);
-	vector <vector <int>> data;
-	if (!infile){
-		cout << "Error reading file!";
-		return;
-	}
-
-	//read the comma separated values into a vector of vector of ints
-	while (infile)
-	{
-		string s;
-		if (!getline( infile, s )) break;
-
-		istringstream ss(s);
-		vector <int> datarow;
-
-		while (ss)
-		{
-		  string srow;
-		  int sint;
-		  if (!getline( ss, srow, ',' )) break;
-		  sint = atoi(srow.c_str()); //convert string to int
-		  datarow.push_back(sint);
-		}
-		data.push_back(datarow);
-	}
-
-	//draw red circles on the image
-	for (int i = 0; i < data.size(); ++i)
-	{
-		Point center(data.at(i).at(0), data.at(i).at(1)) ;
-		circle(binary3channel, center, 3, Scalar(0,0,255), -1, 8);
-	}
-		
-
+    //draw red circles on the image
+    for (int i = 0; i < data.size(); ++i)
+    {
+        Point center = data[i].getCurrentPrediction();
+        //cout << data[i].color<< endl;
+        circle(binary3channel, center, 3, data[i].color, -1, 8);
+    }
 }
+
+
